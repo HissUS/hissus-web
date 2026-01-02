4e90af141e31c31d2ded7ce908a05b393bade0d8
pou14 <poyiou814@gmail.com>
2026-01-02 15:50:00 -0600

    Initialize dependencies

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,7 +1,17 @@
+import logo from "../../assets/logo.png";
+
 export default function Navbar() {
   return (
-    <nav>
-      <h1>Navbar</h1>
-    </nav>
+    <header>
+      <img src={logo} alt="HISS US Logo" className="logo" />
+      <nav>
+        <ul>
+          <li><a href="#products">Products</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#values">Our Values</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
