79deac4dd9b57472c69c48fde6fe9a8dc050a9b1
pou14 <poyiou814@gmail.com>
2026-01-07 14:55:32 -0600

    Fixed DaisyUI config error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import daisyui from "daisyui";
+
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // scan all your React components
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [daisyui],
+};
